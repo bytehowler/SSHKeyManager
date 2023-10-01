4349261402fd4d753d119a6ca8be542eb3a09e1d
Luke <28677981+AlfaLynx@users.noreply.github.com>
2023-10-01 23:19:41 +0200

    Added buttons to main GUI Added limited functionality to Add and Remove buttons

--- SSHKeyManager.Designer.cs
+++ SSHKeyManager.Designer.cs
@@ -38,29 +38,33 @@
             // listSSHViewer
             // 
             listSSHViewer.FormattingEnabled = true;
-            listSSHViewer.ItemHeight = 20;
-            listSSHViewer.Location = new Point(12, 54);
+            listSSHViewer.ItemHeight = 15;
+            listSSHViewer.Location = new Point(10, 40);
+            listSSHViewer.Margin = new Padding(3, 2, 3, 2);
             listSSHViewer.Name = "listSSHViewer";
-            listSSHViewer.Size = new Size(776, 384);
+            listSSHViewer.Size = new Size(680, 289);
             listSSHViewer.TabIndex = 1;
             listSSHViewer.SelectedIndexChanged += listBox1_SelectedIndexChanged;
             // 
             // buttonAdd
             // 
-            buttonAdd.Location = new Point(12, 12);
+            buttonAdd.Location = new Point(10, 9);
+            buttonAdd.Margin = new Padding(3, 2, 3, 2);
             buttonAdd.Name = "buttonAdd";
-            buttonAdd.Size = new Size(94, 36);
+            buttonAdd.Size = new Size(82, 27);
             buttonAdd.TabIndex = 2;
             buttonAdd.Text = "Add";
             buttonAdd.UseVisualStyleBackColor = true;
+            buttonAdd.Click += SSHKeyManager_Add;
             // 
             // buttonEdit
             // 
             buttonEdit.Enabled = false;
             buttonEdit.FlatStyle = FlatStyle.System;
-            buttonEdit.Location = new Point(112, 12);
+            buttonEdit.Location = new Point(98, 9);
+            buttonEdit.Margin = new Padding(3, 2, 3, 2);
             buttonEdit.Name = "buttonEdit";
-            buttonEdit.Size = new Size(94, 36);
+            buttonEdit.Size = new Size(82, 27);
             buttonEdit.TabIndex = 3;
             buttonEdit.Text = "Edit";
             buttonEdit.UseVisualStyleBackColor = true;
@@ -68,18 +72,20 @@
             // buttonRemove
             // 
             buttonRemove.Enabled = false;
-            buttonRemove.Location = new Point(694, 12);
+            buttonRemove.Location = new Point(607, 9);
+            buttonRemove.Margin = new Padding(3, 2, 3, 2);
             buttonRemove.Name = "buttonRemove";
-            buttonRemove.Size = new Size(94, 36);
+            buttonRemove.Size = new Size(82, 27);
             buttonRemove.TabIndex = 4;
             buttonRemove.Text = "Remove";
             buttonRemove.UseVisualStyleBackColor = true;
+            buttonRemove.Click += SSHKeyManager_Remove;
             // 
             // SSHKeyManager
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
             Controls.Add(buttonRemove);
             Controls.Add(buttonEdit);
             Controls.Add(buttonAdd);
@@ -87,6 +93,7 @@
             FormBorderStyle = FormBorderStyle.FixedSingle;
             HelpButton = true;
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 2, 3, 2);
             MaximizeBox = false;
             Name = "SSHKeyManager";
             Text = "SSH Key Manager";
--- SSHKeyManager.cs
+++ SSHKeyManager.cs
@@ -14,7 +14,18 @@ namespace SSHKeyManager
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            buttonRemove.Enabled = true;
+        }
 
+        private void SSHKeyManager_Add(object sender, EventArgs e)
+        {
+            //(sender as Button)!.Text = "A";
+            listSSHViewer.Items.Add("Lorem Ipsum");
+        }
+
+        private void SSHKeyManager_Remove(object sender, EventArgs e)
+        {
+            listSSHViewer.Items.Remove(listSSHViewer.SelectedItem!);
         }
     }
 }
